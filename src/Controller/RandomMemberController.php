bbb2ebb3018209800f2a9fb49b43ad68640c0f14
Mladen Salaj <mladensalaj@Mladens-MBP.fritz.box>
2017-12-29 12:30:26 +0100

    Installs TWIG, adds and tests a template

--- src/Controller/RandomMemberController.php
+++ src/Controller/RandomMemberController.php
@@ -6,7 +6,11 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RandomMemberController {
+// extends Symfony's base Controller class:
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class RandomMemberController extends Controller {
 
   /**
   * @Route("/random-member")
@@ -19,12 +23,18 @@ class RandomMemberController {
     $pickedMember = $memberContainer[ array_rand( $memberContainer ) ];
 
 
-    return new Response(
-      '<html>
-        <body>
-          <h1>' . $pickedMember . '</h1>
-        </body>
-      </html>'
-    );
+    // return new Response(
+    //   '<html>
+    //     <body>
+    //       <h1>' . $pickedMember . '</h1>
+    //     </body>
+    //   </html>'
+    // );
+
+    return $this->render('random-member.html.twig', array(
+      'pickedMember' => $pickedMember,
+    ));
+
+
   }
 }
